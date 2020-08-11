ba215d8370f8e8f7155e496f672c29539275fb5a
Joram van den Boezem <joram@vandenboezem.nl>
2020-08-11 10:33:42 +0200

    feat: various

--- healthcheck.cmd
+++ healthcheck.cmd
@@ -1,1 +1,4 @@
-curl --connect-timeout 5 --max-time 60 --retry 1 --user guest:guest http://localhost:15672/api/healthchecks/node
+REM curl --connect-timeout 5 --max-time 60 --retry 1 --user guest:guest http://localhost:15672/api/healthchecks/node
+
+cd "C:/Program Files/RabbitMQ Server/rabbitmq_server-%VERSION%/sbin"
+rabbitmqctl.bat ping
